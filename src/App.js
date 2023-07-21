5d7aee5e2e1f1c2b7b9ba63f5641b0f7d03c0508
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-21 22:10:33 +0800

    Add imports

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { React, useState, useRef } from 'react'
+import uniqid from 'uniqid'
+import pencilEdit from './assets/pencil-edit.svg'
+import trashCan from './assets/trash-can.svg'
+import githubLogo from './assets/github-logo.svg'
+import html2canvas from 'html2canvas'
+import jsPDF from 'jspdf'
+import './css/main.css'
