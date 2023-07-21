60d3c70c3de8e08f127fe3b0dfb4babc4c47e990
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-21 22:51:29 +0800

    Change import createRoot to ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './css/main.css'
 
 document.body.innerHTML = '<div id="root"></div>';
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
