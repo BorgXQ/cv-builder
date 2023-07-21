2a7a502192b5e5c86a2a0896c4cc5c9208df68bf
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-21 22:08:20 +0800

    Add import main.css and App.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.js'
+import './css/main.css'
 
 document.body.innerHTML = '<div id="root"></div>';
 
